5bdc9435748aef40e2d1339dc7ad6b363cd3a6e3
kdempire <128413009+Karandhawariya@users.noreply.github.com>
2024-06-27 20:59:23 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -2,6 +2,7 @@ resource "google_compute_instance" "default" {
   name         = "my-instance"
   machine_type = "n2-standard-2"
   zone         = "us-central1-a"
+  project      = "ayush-424308"
 
   boot_disk {
     initialize_params {
