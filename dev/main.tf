c37a7a38fa3d016e9fa20eaa14c189b14d33af7f
kdempire <128413009+Karandhawariya@users.noreply.github.com>
2024-11-05 21:57:25 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -1,23 +1,3 @@
-resource "google_compute_instance" "default" {
-  name         = "my-instance"  
-  machine_type = "n2-standard-2"
-  zone         = "us-central1-a"  
-  project      = "kd-project-437712"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
- 
-    }
-  } 
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
+module "instance" {
+  source = "github.com/Karandhawariya/quest//demo"  
 }
-
-
