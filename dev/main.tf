2fb44a55c62d5040a1f2bdd6dbba1f699fedf945
kdempire <128413009+Karandhawariya@users.noreply.github.com>
2024-11-05 23:34:45 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -1,4 +1,4 @@
 module "instance" {
-  #source = "https://github.com/Karandhawariya/quest.git"
-  source = "git@github.com:karandhawariya/quest.git"
+  source = "https://github.com/Karandhawariya/quest.git"
+  #source = "git@github.com:karandhawariya/quest.git"
 }
